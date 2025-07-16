6be916c2e45c21214ae3359bfe567a8bd694181c
SergioGBosco <sergiogibosco@gmail.com>
2025-07-16 17:19:26 +0200

    feat: add fuction for generate card and use cicle for to add html elements into the layout

--- script.js
+++ script.js
@@ -36,3 +36,40 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+//Primo passaggio 
+//Recupero gli elementi dell'html che devo andare ad utilizzare
+
+const memberTeam = document.getElementById(`memberTeam`);
+
+
+//ciclo l'array per andare ad inserire la struttura html per ogni elemento 
+function generatecardteam(descrition) {
+  let membercard = `<div class="col-12 col-md-4">
+  <div class="card mb-3">
+  <div class="row g-0">
+  <div class="col-md-4">
+  <img src="${descrition.img}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8 bg-black">
+                  <div class="card-body  text-white ">
+                  <h5 class="card-title" id="name">${descrition.name}</h5>
+                  <p class="card-text" id="role">${descrition.role}</p>
+                  <a class="card-text" id="email">${descrition.email}</a>
+                  </div>
+                  </div>
+                  </div>
+                  </div>
+                  </div>`
+  return membercard
+}
+
+//Ciclo ogni elemento presente nell'array di oggetti per inserirlo all'interno dell'html tramite il collegamento prima richiamato
+for (let i = 0; i < teamMembers.length; i++) {
+  let member = generatecardteam(teamMembers[i]);
+
+  memberTeam.innerHTML += member
+}
+
+
+
